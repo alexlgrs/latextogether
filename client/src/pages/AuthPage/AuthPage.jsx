5603c28e56696398e6e84bfcbc9fc16b517091f2
alexlgrs <alex.lgrs22@gmail.com>
2026-02-19 08:28:42 +0100

    style: Ajout style sur authPage

--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -1,13 +1,38 @@
-import Login from "../../components/LoginComponent/Login"
-import Register from "../../components/RegisterComponent/Register"
+import { useState } from 'react';
+import Login from "../../components/LoginComponent/Login";
+import Register from "../../components/RegisterComponent/Register";
+import "./AuthPage.css";
 
 const AuthPage = () => {
+  const [isLoginView, setIsLoginView] = useState(true);
+
   return (
-    <div>
-      <div>page d'auth</div >
-      
-      <Login/>
-      <Register/>
+    <div className='AuthPage'>
+      <div className='authContainer'>
+        <div className='authHeader'>
+          <h1>LatexTogether</h1>
+          <p>Collaborez sur vos documents LaTeX en temps réel</p>
+        </div>
+
+        <div className='authToggle'>
+          <button 
+            className={isLoginView ? 'active' : ''} 
+            onClick={() => setIsLoginView(true)}
+          >
+            Connexion
+          </button>
+          <button 
+            className={!isLoginView ? 'active' : ''} 
+            onClick={() => setIsLoginView(false)}
+          >
+            Inscription
+          </button>
+        </div>
+
+        <div className='authContent'>
+          {isLoginView ? <Login /> : <Register />}
+        </div>
+      </div>
     </div>
   );
 };
